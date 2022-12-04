17d2007f2e9f8276a238b82c1466fb6c2553ab62
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-12-04 09:40:34 +0200

    updated

---
+++ springboot-backend/springboot-backend/src/main/java/com/example/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.example.springbootbackend.repository;
+
+import com.example.springbootbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("SELECT e FROM Employee e WHERE e.email = ?1")
+    Optional<Employee> findEmployeeByEmail(String email);
+
+    boolean existsByEmail(String email);
+    Optional<Employee> findByEmail(String email);
+}
