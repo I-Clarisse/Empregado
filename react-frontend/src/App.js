18150cb91965621b35112498f88d6fba1038a187
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-12-04 09:39:07 +0200

    updated

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Footer from './components/Footer'
+import Header from './components/Header'
+import CreateEmployee from './components/CreateEmployee'
+import ListEmployee from './components/ListEmployee';
+import ViewEmployee from './components/ViewEmployee';
 
 function App() {
   return (
-    <p>Welcome To Empregado</p>
+    <div>
+      <Router>
+        <Header/>
+        <div className='container'>
+          <Routes>
+            {/* <Route path='/' element = {<ListEmployee/>}/> */}
+            <Route path='/' element = {<CreateEmployee/>}/>
+            <Route path='/employee' element = {<ListEmployee/>}/>
+            {/* <Route path='/add-employee' element = {<CreateEmployee/>}/> */}
+            {/* <Route path='/add-employee/_add' element = {<CreateEmployee/>}/> */}
+            <Route path='/view-employee/:id' element = {<ViewEmployee/>}/>
+          </Routes>
+        </div>
+        <Footer/>
+      </Router>
+      </div>
   );
 }
 
