1a12b779d55d98d57c46eb2f05f8a0d9da174bfb
I-Clarisse <79416565+I-Clarisse@users.noreply.github.com>
2022-12-07 20:57:57 +0200

    updates

---
+++ react-frontend/src/components/homepage/homepage.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import './homepage.css';
+
+class Homepage extends Component{
+    render() {
+        return(
+            <div>
+                <h1 className="welcome">
+                    Welcome to <span>EmPREGADO</span>
+                </h1>
+            </div>
+        )
+    }
+}
+export default Homepage;
